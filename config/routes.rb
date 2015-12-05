bd88bb12de85726b04be0f1a1d2af15927ee182b
Jongmin Kim <kjm141215@gmail.com>
2015-12-05 14:00:06 +0900

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :sales
   resources :products
-  resources :products
-  devise_for :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
